498c29559480d8383fc608bf00d81a55a17d3661
darshilagrawal <darshilagrawal2000@gmail.com>
2021-03-11 23:32:02 +0530

    Practice 8

---
+++ react-components/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import Lists from "./Lists";
+ReactDOM.render(
+  <div>
+    <Heading />
+    <Lists />
+  </div>,
+  document.getElementById("root")
+);
